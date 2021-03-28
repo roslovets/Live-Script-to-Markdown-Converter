3bc6bf3b8e26b0e716d06a36a94df1d554d4cfca
Pavel Roslovets <roslovets@exponenta.ru>
2021-03-29 01:44:46 +0300

    Fix installweb

--- main/installer/installweb.m
+++ main/installer/installweb.m
@@ -1,4 +1,4 @@
-TEMP_INST.uuid = '';
+TEMP_INST.uuid = 'c5ce6999-1e66-4758-81ee-567db8689a9b';
 TEMP_INST.url = matlab.addons.repositories.FileExchangeRepository().getAddonURL(TEMP_INST.uuid);
 TEMP_INST.fname = websave(tempname + ".mltbx", TEMP_INST.url);
 TEMP_INST.info = matlab.addons.toolbox.installToolbox(TEMP_INST.fname);
